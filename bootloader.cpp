bae10c5717b9aac1f18c93a6fb471cd7cfeccca7
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-03 05:12:16 +0400

    .

---
+++ bootloader.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "kernel/kernel.h"
+#include "kernel/extra/initramfs.h"
+#include "kernel/extra/make_dirs.h"
+#include <filesystem>
+void bootloader(int argc, char* argv[]){
+    
+}
