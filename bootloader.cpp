5061090f88d38df482ae077bd73dcec80aa58331
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-03 05:18:57 +0400

    the lion does not concern himself with git commit messages

--- bootloader.cpp
+++ bootloader.cpp
@@ -4,5 +4,9 @@
 #include "kernel/extra/make_dirs.h"
 #include <filesystem>
 void bootloader(int argc, char* argv[]){
-    
+    std::string boot_option;
+    std::cout << "Flash Bootloader\n";
+    std::cout << "Talon : [1]\n";
+    std::cout << "Enter Boot Option \n --> ";
+    std::cin >> boot_option;
 }
