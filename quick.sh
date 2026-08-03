0dc53577131ac1842874d41e32b4c91f3bb80ee5
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-04 01:33:55 +0400

    the lion does not concern himself with git commit messages

--- quick.sh
+++ quick.sh
@@ -1,1 +1,2 @@
-g++ -o main  main.cpp bootloader.cpp kernel/extra/make_dirs.cpp logo.cpp update.cpp kernel/kernel.c
+SRC="main.cpp bootloader.cpp kernel/extra/make_dirs.cpp logo.cpp update.cpp kernel/kernel.c"
+g++ -o main $SRC
