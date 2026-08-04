73b332504f38d02d9a60d192468b6951e84dacc1
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-04 05:18:55 +0400

    the lion does not concern himself with git commit messages

--- main.cpp
+++ main.cpp
@@ -9,10 +9,20 @@
 #include <sstream>
 #include <string>
 #include <filesystem>
-std::string version = "V0.0.1";
+std::string version = "V0.0.2";
 std::string home_dir = "filesystem/home/admin/Desktop";
 std::string cache_dir = "filesystem/home/admin/.cache";
 std::string config_dir = "filesystem/home/admin/.config";
+std::string read_file(const std::string& filepath) {
+    std::ifstream file(filepath);
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    return buffer.str();
+}
+void write_file(const std::string& filepath, const std::string& content) {
+    std::ofstream file(filepath);
+    file << content;
+}
 int main(int argc,char* argv[]){
 bootloader(argc,argv);
    std::string shell;
@@ -42,6 +52,24 @@ bootloader(argc,argv);
      else if (shell.substr(0,4) == "echo"){
       if (shell.length() > 5){
          std::cout << shell.substr(5) << "\n";
+      }
+      else{
+         std::cout << "echo what?\n";
+      }
+     }
+     else if (shell == "date"){
+      std::system("date > filesystem/home/admin/.cache/date.txt");
+      std::string date_cmd = read_file("filesystem/home/admin/.cache/date.txt");
+      std::cout << date_cmd << "\n";
+     }
+     else if (shell.substr(0,5) == "which"){
+      if (shell.length() > 6){
+        std::string which_full_form = shell.substr(6);
+        std::string which_full_command = "find filesystem/bin/ -name \"" + which_full_form + "\"";
+        std::system(which_full_command.c_str());
+      }
+      else{
+         std::cout << "say a filename\n";
       }
      }
    }
