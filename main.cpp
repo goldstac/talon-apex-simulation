39e2f2e850f768756c3fc98522ac14093212e5c1
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-06 10:17:19 +0400

    update

--- main.cpp
+++ main.cpp
@@ -134,7 +134,7 @@ bootloader(argc,argv);
       std::cout << "-----------------------------\n";
       std::cout << "Talon Apex " << version << "\n";
       std::cout << "-------------------------------";
-      std::cout << "";
+      std::cout << "Kernel | Talon Apex Kernel " << kernel_version << "\n";
      }
    }
    
