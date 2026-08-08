3c9bcf8bb07283143b67fb04be9b31fdc29c4f1a
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-08 06:58:32 +0400

    add

---
+++ extra/push.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+int main(){
+    std::string input;
+    std::string commit_msg;
+    std::string full;
+    std::cout << "Welcome To Push\n";
+    std::cout << "Options \n";
+    std::cout << "[1] Add . , Custom Commit , Push main\n";
+    std::cout << "Enter Choice\n --> ";
+    std::cin >> input;
+    if (input == "1"){
+        std::cout << "Enter Commit Message\n --> ";
+        std::cin >> commit_msg;
+        full = "git add . && git commit -m \"" + commit_msg + "\"";
+        std::system(full.c_str());
+        std::cout << "done :) \n";
+    }
+}
