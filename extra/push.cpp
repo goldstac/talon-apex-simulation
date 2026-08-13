352614e0ab619ee9ed54d4f44818184d6f1e4c5a
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-13 21:17:12 +0400

    fix push.cpp

--- extra/push.cpp
+++ extra/push.cpp
@@ -1,20 +1,30 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
-int main(){
+
+int main() {
     std::string input;
     std::string commit_msg;
+    std::string branch_name;
     std::string full;
+
     std::cout << "Welcome To Push\n";
     std::cout << "Options \n";
-    std::cout << "[1] Add . , Custom Commit , Push main\n";
+    std::cout << "[1] Add . , Custom Commit, Push to Branch\n";
     std::cout << "Enter Choice\n --> ";
     std::cin >> input;
-    if (input == "1"){
+
+    if (input == "1") {
         std::cin.ignore();
+
+        std::cout << "Enter Branch Name\n --> ";
+        std::getline(std::cin, branch_name);
+
         std::cout << "Enter Commit Message\n --> ";
-        std::getline(std::cin,commit_msg);
-        full = "git add . && git commit -m \"" + commit_msg + "\" && git push";
+        std::getline(std::cin, commit_msg);
+
+        full = "git add . && git commit -m \"" + commit_msg + "\" && git push origin " + branch_name;
+
         std::system(full.c_str());
         std::cout << "done :) \n";
     }
