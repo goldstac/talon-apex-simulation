0fce46848d741e1bed51119d3a33743467bc1f3e
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-15 13:09:44 +0400

    basic implementation of calculator.c

--- extra/apex-dep/calculator.c
+++ extra/apex-dep/calculator.c
@@ -1,5 +1,18 @@
+#include <string.h>
 #include <stdio.h>
 int main(){
-    printf("Calculator\n");
-    return 0;
+   int first_number;
+   int second_number;
+   int final_calculation;
+   char operator[3];
+   printf("----------------------\n");
+   printf("   Talon Calculator\n");
+   printf("----------------------\n");
+   printf("Enter Operator > ");
+   scanf("%s",operator);
+   printf("Operator %s Selected\n",operator);
+   if (strcmp(operator,"+") == 0){
+
+   }
+
 }
