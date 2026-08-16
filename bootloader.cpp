fcee029889813535ff83d9dbdbf69784f3b57c7d
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-08-16 12:25:39 +0400

    fix boot option arguments

--- bootloader.cpp
+++ bootloader.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
 #include "kernel/kernel.h"
 #include "kernel/extra/initramfs.h"
 #include "kernel/extra/make_dirs.h"
 void bootloader(int argc, char* argv[]){
-    int boot_option;
+    std::string boot_option;
     std::cout << "Flash Bootloader\n";
     std::cout << "Talon : [1]\n";
     std::cout << "Enter Boot Option \n --> ";
-    std::cin >> boot_option;
-    std::cin.ignore();
-    if (boot_option == 1){
-        make_dirs(argc,argv);
-        create_tmpfs_initramfs();
-        kernel();
+    std::getline(std::cin, boot_option);
+    if (boot_option != "1"){
+        std::cout << "Invalid boot option. Only [1] is supported.\n";
+        std::exit(0);
     }
+    make_dirs(argc,argv);
+    create_tmpfs_initramfs();
+    kernel();
 }
