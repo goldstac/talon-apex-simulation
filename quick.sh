88cc78a6994365a42d4a1598d8a4b4600490d5b5
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-09-02 23:29:47 +0400

    reset

--- quick.sh
+++
@@ -1,2 +0,0 @@
-SRC="main.cpp bootloader.cpp kernel/extra/make_dirs.cpp kernel/extra/initramfs.cpp kernel/extra/logger.cpp logo.cpp update.cpp kernel/kernel.c extra/apex-dep/calculator.c"
-g++ -o main $SRC
--- tag.sh
+++
@@ -1,4 +0,0 @@
-echo "Welcome To Tag"
-echo "Enter Tag Version -->"
-read version
-git tag $version && git push origin $version
