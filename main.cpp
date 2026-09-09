11d0984efc826616ef18e3165251775e3c61e178
Li Productions <172457372+goldstac@users.noreply.github.com>
2026-09-09 16:03:29 +0400

    feat(shell): add exit command

--- main.cpp
+++ main.cpp
@@ -7,9 +7,15 @@ int main() {
     std::string version = "0.0.1";
     std::string status = "Alpha";
     std::string shell;
-    std::cout << "admin@talon [~/Desktop] [ " << version << " " << status << " ] \n > ";
-    std::getline(std::cin,shell);
-    if (shell == "t!ping"){
-        std::cout << "Pong!\n";
+    while (1){
+        std::cout << "admin@talon [~/Desktop] [ " << version << " " << status << " ] \n > ";
+        std::getline(std::cin,shell);
+        if (shell == "exit"){
+            break;
+        }
+        else if (shell == "t!ping"){
+            std::cout << "Pong!";
+        }
     }
+
 }
